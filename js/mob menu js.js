561b42f360e9db65c87bcfbcc01dc725dba85dcd
Aleksandr Cherepanov <cherepanov3496@gmail.com>
2022-04-28 13:27:28 +0300

    добавление js кода для появление бокового меню

---
+++ js/mob menu js.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('.burger');
+let menuBtn = document.querySelector('.leftside');
+
+menu.addEventListener('click', function(){
+    menuBtn.classList.toggle('show');
+})
