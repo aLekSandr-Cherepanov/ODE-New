97590b7ebd1879c220399ecdd45c6ec4e6e0d738
Aleksandr Cherepanov <cherepanov3496@gmail.com>
2022-04-28 14:49:46 +0300

    сделал скрытие меню и добавил крест

--- js/mob menu js.js
+++ js/mob menu js.js
@@ -1,6 +1,11 @@
 let menu = document.querySelector('.burger');
 let menuBtn = document.querySelector('.leftside');
+let cross = document.querySelector('.cross-mobmenu')
 
 menu.addEventListener('click', function(){
+    menuBtn.classList.toggle('show');
+})
+
+cross.addEventListener('click', function(){
     menuBtn.classList.toggle('show');
 })
