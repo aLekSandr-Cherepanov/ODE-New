521cbe6150fcae8a78079ef3e23b0c4fd98f1376
Alexander Cherepanov <super.sany96@yandex.ru>
2022-07-11 14:58:27 +0300

    работает вывод числа как строка,чтобы выводилось как число пришлось обернуть числа цены в span

--- написание новой корзины/js/cart.js
+++ написание новой корзины/js/cart.js
@@ -1,6 +1,6 @@
 const productsBtn = document.querySelectorAll ('.product__btn');
 const cartProductsList = document.querySelector('.cart-content__list');
-const cart = document.querySelector('.cart');
+const cart = document.querySelector('.cerd');
 const cartQuantity = document.querySelector('.cart__quantity');
 const fullPrice = document.querySelector('.fullprice');
 let price = 0;
